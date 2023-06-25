0607dee596ef0d59cbf317bd3eaaff90665a72f8
AaronCS25 <aaron.camacho@utec.edu.pe0>
2023-06-25 14:06:47 -0500

    padecimientos_screen.dart: added (without functions)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:care_file_one/src/screens/alergias_screen.dart';
 import 'package:care_file_one/src/screens/home_screen.dart';
 import 'package:care_file_one/src/screens/login_screen.dart';
 import 'package:care_file_one/src/screens/medicacion_screen.dart';
+import 'package:care_file_one/src/screens/padecimientos_screen.dart';
 import 'package:care_file_one/src/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -18,13 +19,14 @@ class MainApp extends StatelessWidget {
       title: 'CareFile-One',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      initialRoute: '/medicacion',
+      initialRoute: '/padecimientos',
       routes: {
         '/': (context) => const LoginScreen(),
         '/signup': (context) => const SignUpScreen(),
         '/home': (context) => const HomeScreen(),
         '/alergias': (context) => const AlergiasScreen(),
         '/medicacion': (context) => const MedicacionScreen(),
+        '/padecimientos': (context) => const PadecimientosScreen(),
       },
     );
   }
---
+++ lib/src/screens/padecimientos_screen.dart
@@ -0,0 +1,54 @@
+import 'package:care_file_one/src/widgets/main_card.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PadecimientosScreen extends StatefulWidget {
+  const PadecimientosScreen({super.key});
+
+  @override
+  State<PadecimientosScreen> createState() => _PadecimientosScreenState();
+}
+
+class _PadecimientosScreenState extends State<PadecimientosScreen> {
+  List<Map<String, String>> alergias = [
+    {'title': 'Diabetes', 'type': 'tratado'},
+    {'title': 'Artritis', 'type': 'no_tratado'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          'PADECIMIENTOS',
+          style: GoogleFonts.montserrat(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            padding: const EdgeInsets.only(right: 10),
+            icon: Image.asset(
+              'assets/icons/add_icon.png',
+              height: 32,
+              width: 32,
+            ),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: alergias.length,
+        itemBuilder: (context, index) {
+          return MainCard(
+            title: alergias[index]['title']!,
+            type: alergias[index]['type']!,
+            section: 'padecimientos',
+          );
+        },
+      ),
+    );
+  }
+}

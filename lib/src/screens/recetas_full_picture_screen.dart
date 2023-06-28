24fd23c65c35a4dd29efb90b5c011babf067c749
AaronCS25 <aaron.camacho@utec.edu.pe0>
2023-06-28 01:35:38 -0500

    nothing

--- lib/src/screens/recetas_full_picture_screen.dart
+++ lib/src/screens/recetas_full_picture_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:path/path.dart';
 
 class RecetaFullPictureScreen extends StatefulWidget {
   const RecetaFullPictureScreen({super.key, required this.url});

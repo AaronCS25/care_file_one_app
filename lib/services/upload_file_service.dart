916b3a825a8df8ff405f5a7fda42ecc14967a35f
AaronCS25 <aaron.camacho@utec.edu.pe0>
2023-06-28 01:50:36 -0500

    services: updated

--- lib/services/upload_file_service.dart
+++ lib/services/upload_file_service.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:care_file_one/services/crypto_name_services.dart';
 import 'package:care_file_one/services/s3_service.dart';
+import 'package:care_file_one/services/auth_service.dart';
 import 'package:path/path.dart' as path;
 
 class UploadFileService {
@@ -10,10 +11,11 @@ class UploadFileService {
     final CryptoNameService cryptoNameService = CryptoNameService();
 
     try {
+      final String userIdString = await Auth.getUserId();
       String fileName = path.basename(file.path);
       String fileExtension = path.extension(file.path);
       String fileNameCrypto =
-          cryptoNameService.encryptName(fileName, fileExtension, '1');
+          cryptoNameService.encryptName(fileName, fileExtension, userIdString);
       await s3uploadService.uploadFile(file, fileNameCrypto);
       return fileNameCrypto;
     } catch (error) {
